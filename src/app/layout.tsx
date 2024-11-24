328f329a94800ac0cd8626ec24076e83becbb785
setvect <setvect@naver.com>
2024-11-24 21:01:53 +0900

    제목, 파비콘 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import { Typography } from "@mui/material";
 import ThemeWrapper from "./components/ThemeWrapper";
 import Link from "next/link";
 
+export const metadata = {
+  title: "복슬금융 계산기",
+  description: "금융 계산을 도와주는 계산기 모음",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
